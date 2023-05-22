e446a1fc44c2d14cea653dc85b141e23aac92c68
yutwins <yutwo0717@gmail.com>
2023-05-22 17:20:37 +0900

    [dev]~except Schedule Section

--- js/scroll.js
+++ js/scroll.js
@@ -10,67 +10,123 @@
   // }
 
   // 要素が一部でも画面内に入ったらクラスを付与する
+  // function isElementInViewport(el) {
+  //   const rect = el.getBoundingClientRect();
+  //   return (
+  //     rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+  //     rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+  //     rect.bottom > 0 &&
+  //     rect.right > 0
+  //   );
+  // }
+  
+  
+  // function addScrollClassToElementA() {
+  //   const elementsA = document.querySelectorAll('.concept__head');
+    
+  //   elementsA.forEach((elementA) => {
+  //     if (isElementInViewport(elementA) && !elementA.classList.contains('visible')) {
+  //       elementA.classList.add('visible');
+  //     }
+  //   });
+  // }
+  
+  // function addClassToChildElements() {
+  //   const elements = document.querySelectorAll('.concept__msg');
+    
+  //   elements.forEach((element) => {
+  //     if (isElementInViewport(element)) {
+  //       element.childNodes.forEach((child) => {
+  //         // Check if the child node is an element node
+  //         if (child.nodeType === Node.ELEMENT_NODE) {
+  //           if (!child.classList.contains('visible')) {
+  //             child.classList.add('visible');
+  //           }
+  //         }
+  //       });
+  //     }
+  //   });
+  // }
+
+  // function addScrollClassToConceptFloat() {
+  //   const ConceptFloat = document.querySelector('.concept-floatings');
+    
+  //   if (isElementInViewport(ConceptFloat) && !ConceptFloat.classList.contains('visible')) {
+  //     ConceptFloat.classList.add('visible');
+  //   }
+  // }
+
+  // // contentsセクションの要素が画面内に入ったらクラスを付与する
+  // function addScrollClassToContentItem() {
+  //   const contentsItems = document.querySelectorAll('.contents-item');
+    
+  //   contentsItems.forEach((contentsItem) => {
+  //     if (isElementInViewport(contentsItem) && !contentsItem.classList.contains('visible')) {
+  //       contentsItem.classList.add('visible');
+  //     }
+  //   });
+  // }
+
+  // function addScrollClassToConceptFloat() {
+  //   const ConceptFloat = document.querySelector('.performer__bg--left');
+    
+  //   if (isElementInViewport(ConceptFloat) && !ConceptFloat.classList.contains('visible')) {
+  //     ConceptFloat.classList.add('visible');
+  //   }
+  // }
+  
+  // window.addEventListener('scroll', () => {
+  //   addScrollClassToElementA();
+  //   addClassToChildElements();
+  //   addScrollClassToConceptFloat();
+  //   addScrollClassToContentItem();
+  // });
+  
+
   function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-      rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
-      rect.bottom > 0 &&
-      rect.right > 0
+        rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+        rect.bottom > 0 &&
+        rect.right > 0
     );
-  }
-  
-  
-  function addScrollClassToElementA() {
-    const elementsA = document.querySelectorAll('.concept__head');
-    
-    elementsA.forEach((elementA) => {
-      if (isElementInViewport(elementA) && !elementA.classList.contains('visible')) {
-        elementA.classList.add('visible');
-      }
-    });
-  }
-  
-  function addClassToChildElements() {
-    const elements = document.querySelectorAll('.concept__msg');
-    
+}
+
+function addVisibilityClass(selector, isChild=false) {
+    const elements = document.querySelectorAll(selector);
     elements.forEach((element) => {
-      if (isElementInViewport(element)) {
-        element.childNodes.forEach((child) => {
-          // Check if the child node is an element node
-          if (child.nodeType === Node.ELEMENT_NODE) {
-            if (!child.classList.contains('visible')) {
-              child.classList.add('visible');
+        if (isElementInViewport(element)) {
+            if(isChild){
+                element.childNodes.forEach((child) => {
+                    if (child.nodeType === Node.ELEMENT_NODE && !child.classList.contains('visible')) {
+                        child.classList.add('visible');
+                    }
+                });
+            } else if(!element.classList.contains('visible')) {
+                element.classList.add('visible');
             }
-          }
-        });
-      }
+        }
     });
-  }
+}
 
-  function addScrollClassToConceptFloat() {
-    const ConceptFloat = document.querySelector('.concept-floatings');
-    
-    if (isElementInViewport(ConceptFloat) && !ConceptFloat.classList.contains('visible')) {
-      ConceptFloat.classList.add('visible');
-    }
-  }
-
-  // contentsセクションの要素が画面内に入ったらクラスを付与する
-  function addScrollClassToContentItem() {
-    const contentsItems = document.querySelectorAll('.contents-item');
-    
-    contentsItems.forEach((contentsItem) => {
-      if (isElementInViewport(contentsItem) && !contentsItem.classList.contains('visible')) {
-        contentsItem.classList.add('visible');
-      }
-    });
-  }
-  
-  window.addEventListener('scroll', () => {
-    addScrollClassToElementA();
-    addClassToChildElements();
-    addScrollClassToConceptFloat();
-    addScrollClassToContentItem();
+window.addEventListener('scroll', () => {
+    addVisibilityClass('.section__title');
+    addVisibilityClass('.concept__head');
+    addVisibilityClass('.concept__msg', true);
+    addVisibilityClass('.concept-floatings');
+    addVisibilityClass('.contents-item');
+    addVisibilityClass('.performer__bg--left');
+    addVisibilityClass('.performer__bg--right');
+    addVisibilityClass('.performer-floatings');
+    addVisibilityClass('.performer-list');
+    addVisibilityClass('.speaker__bg--left');
+    addVisibilityClass('.speaker__bg--right');
+    addVisibilityClass('.speaker-floatings');
+    addVisibilityClass('.speaker-list');
+    addVisibilityClass('.layout-floatings');
+    addVisibilityClass('.team__subTitle');
+    addVisibilityClass('.team-floatings');
+    addVisibilityClass('.team-wrap');
+    addVisibilityClass('.overview-inner');
   });
-  
